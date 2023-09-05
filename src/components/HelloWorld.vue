69c8c2bc72e69b81efd75003602be83d03520214
oLNidfwworld <kuvetov@bk.ru>
2023-09-05 17:22:02 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+  message: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <h1>{{ props.message }}</h1>
+
+  <div class="card">
+    <p>
+      builded and deployed
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
